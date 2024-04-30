e236276e65ab4a414f04d20cacb61593d802a092
JonesJonh <143019996+JonesJonh@users.noreply.github.com>
2024-04-30 11:26:20 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,15 @@
+class Calebe{
+    constructor (twitch) {
+        this.twitch = twitch; // variável de interface
+        //ações
+    }
+
+    metodo1 (exibirTwitch) {
+        //ações
+        window.open(this.twitch);
+    }
+
+    metodo2 () {
+        //ações
+    }
+}
